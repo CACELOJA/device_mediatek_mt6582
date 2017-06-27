51bde5fe3bf60f3721b8ed7b154d309307c6a42f
CACELOJA <Jmsbrn33@gmail.com>
2017-06-27 11:20:51 -0500

    zte_e10q

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,12 +5,12 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/mediatek/mt6582/AndroidBoardVendor.mk
+-include vendor/zte/e10q/AndroidBoardVendor.mk
 
 # copy keylayout overrides
 $(shell mkdir -p $(TARGET_OUT_KEYLAYOUT); \
-    cp -pf device/mediatek/mt6582/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
+    cp -pf device/zte/e10q/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
 
 # copy permissions overrides
 $(shell mkdir -p $(TARGET_OUT_ETC)/permissions; \
-    cp -pf device/mediatek/mt6582/configs/android.hardware.camera.xml $(TARGET_OUT_ETC)/permissions)
+    cp -pf device/zte/e10q/configs/android.hardware.camera.xml $(TARGET_OUT_ETC)/permissions)
